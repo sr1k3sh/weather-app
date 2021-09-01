20d0a8de5b478c2b4e6e1be737481e3efbf70cd0
Rikesh Shrestha <rikesh.shrestha@itonics.de>
2021-09-01 14:54:37 +0545

    gds

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   backend: {
-    baseURL:"https://api.darksky.net/forecast/8adaa664d67a4ecfcc5aff8a99260f16/27.73835237554173,85.3330807919586"
+    baseURL:"https://api.darksky.net/forecast/8adaa664d67a4ecfcc5aff8a99260f16"
   }
 };

264138cf7e655f736d3c6fc373916dc5a34ecd11
Rikesh Shrestha <rikesh.shrestha@itonics.de>
2021-09-03 09:58:43 +0545

    testing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   backend: {
-    baseURL:"https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/8adaa664d67a4ecfcc5aff8a99260f16"
+    baseURL:"https://peaceful-taiga-49919.herokuapp.com/https://api.darksky.net/forecast/8adaa664d67a4ecfcc5aff8a99260f16"
   }
 };

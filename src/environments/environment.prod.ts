a1bbf70d93bbd4068ee12c41fccb3f0234d77277
sr1k3sh <sr1k3sh@gmail.com>
2022-01-14 12:46:32 +0545

    testing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   backend: {
-    baseURL:"https://peaceful-taiga-49919.herokuapp.com/https://api.darksky.net/forecast/8adaa664d67a4ecfcc5aff8a99260f16"
+    baseURL:"https://cors-anywhere.herokuapp.com/corsdemo/https://api.darksky.net/forecast/8adaa664d67a4ecfcc5aff8a99260f16"
   }
 };
